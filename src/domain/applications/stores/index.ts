e254fc9f41aeb965fbfea2885fbd5df308e005e7
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-17 12:20:55 +0300

    init

--- src/domain/applications/stores/index.ts
+++ src/domain/applications/stores/index.ts
@@ -28,6 +28,25 @@ export const useApplications = defineStore("applications", () => {
     })
   }
 
+  // const dummyFloatRequests: FloatRequest[] = [
+  //   { id: 1, requestDate: "2021-09-01", amount: 10000000, status: "pending", branch: "Branch 1", approvedBy: null },
+  //   { id: 4, requestDate: "2021-09-04", amount: 40000000, status: "pending", branch: "Branch 4", approvedBy: null },
+  //   { id: 2, requestDate: "2021-09-02", amount: 20000000, status: "approved", branch: "Branch 2", approvedBy: "Manager One" },
+  //   { id: 3, requestDate: "2021-09-03", amount: 30000000, status: "rejected", branch: "Branch 3", approvedBy: null },
+  // ];
+
+  // const floatRequests: Ref<FloatRequest[]> = ref(dummyFloatRequests);
+
+  //dummy service requests
+  const dummyServiceRequests: ServiceRequest[] = [
+    { id: 1, requestDate: "2021-09-01", service: "Company Incorporation", status: "pending", applicant: "Till 004"},
+    { id: 2, requestDate: "2021-09-02", service: "Tax Clearance", status: "approved", applicant: "Till 005"},
+    { id: 3, requestDate: "2021-09-03", service: "Company Incorporation", status: "rejected", applicant: "James Kangave"},
+    { id: 4, requestDate: "2021-09-04", service: "Tax Clearance", status: "pending", applicant: "Till 006"},
+  ];
+
+  const serviceRequests: Ref<ServiceRequest[]> = ref(dummyServiceRequests);
+
   const fetchApplicationStats = async (status:boolean) => {
     return api.get("/registry/v1/applications/stats?status="+status).then((response:any) => {
       applicationStatistics.value = response.data.data
