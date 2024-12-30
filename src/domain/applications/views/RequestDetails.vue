571c52cee1360792b4cbab89e9131199ecdd212f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-30 21:18:05 +0300

    restyles dashboard

--- src/domain/applications/views/RequestDetails.vue
+++ src/domain/applications/views/RequestDetails.vue
@@ -11,12 +11,12 @@ function rejectRequest() {
     .then(() => {
       loading.value = false;
       showDeleteModal.value = false;
-      requestLogs.value.push({ method: 'DELETE', status: 'SUCCESS' });
+      requestLogs.value.push({ method: "DELETE", status: "SUCCESS" });
       fetch();
     })
     .catch((error: AxiosError<ApiErrorResponse>) => {
       loading.value = false;
-      requestLogs.value.push({ method: 'DELETE', status: 'FAILURE' });
+      requestLogs.value.push({ method: "DELETE", status: "FAILURE" });
       notify.error(
         error.response?.data.message || "Error deleting the document"
       );
@@ -29,7 +29,11 @@ function rejectRequest() {
     <div class="w-full max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
       <!-- Breadcrumb -->
       <div class="text-sm mb-4 bg-gray-10 py-2 rounded-sm">
-        <router-link to="/mda/service-requests" class="text-gray-500 hover:text-gray-700 ml-2">Service Requests</router-link>
+        <router-link
+          to="/mda/service-requests"
+          class="text-gray-500 hover:text-gray-700 ml-2"
+          >Service Requests</router-link
+        >
         <span class="mx-2 text-gray-400">/</span>
         <span class="text-gray-700 font-semibold">Request Details</span>
       </div>
@@ -40,16 +44,25 @@ function rejectRequest() {
 
         <div class="space-y-2">
           <div>
-            <span class="block font-medium text-gray-500">Tracking Number:</span>
-            <span class="block text-red-700 font-semibold hover:underline cursor-pointer">123456</span>
+            <span class="block font-medium text-gray-500"
+              >Tracking Number:</span
+            >
+            <span
+              class="block text-red-700 font-semibold hover:underline cursor-pointer"
+              >123456</span
+            >
           </div>
           <div>
             <span class="block font-medium text-gray-500">Service:</span>
             <span class="block text-gray-800">Service Name</span>
           </div>
           <div>
-            <span class="block font-medium text-gray-500">Service Description:</span>
-            <span class="block text-gray-800">Description of the requested service.</span>
+            <span class="block font-medium text-gray-500"
+              >Service Description:</span
+            >
+            <span class="block text-gray-800"
+              >Description of the requested service.</span
+            >
           </div>
           <div>
             <span class="block font-medium text-gray-500">Applicant Name:</span>
@@ -72,19 +85,22 @@ function rejectRequest() {
 
       <!-- Action Buttons -->
       <div class="flex justify-center space-x-4 mt-6">
-        <button class="px-4 py-1 bg-amber-600 text-white rounded-md hover:bg-amber-500 transition">
+        <button
+          class="px-4 py-1 bg-amber-600 text-white rounded-md hover:bg-amber-500 transition"
+        >
           <i class="fa fa-exclamation-circle"></i>
           Query
         </button>
         <button
-        @click="
-                        showDeleteModal = true;"
-                        
-        class="px-4 py-1 bg-red-700 text-white rounded-md hover:bg-red-600 transition">
+          @click="showDeleteModal = true"
+          class="px-4 py-1 bg-red-700 text-white rounded-md hover:bg-red-600 transition"
+        >
           <i class="fa fa-times-square"></i>
           Reject
         </button>
-        <button class="px-4 py-1 bg-green-700 text-white rounded-md hover:bg-green-600 transition">
+        <button
+          class="px-4 py-1 bg-green-700 text-white rounded-md hover:bg-green-600 transition"
+        >
           <i class="fa fa-check"></i>
           Approve
         </button>
@@ -106,7 +122,10 @@ function rejectRequest() {
           Are you sure you want to delete this template and lose it completely?
         </p>
         <div class="flex w-1/2 gap-2 justify-center mx-auto">
-          <button class="bg-blue-400 hover:bg-blue-500 w-1/2 rounded text-white" @click="showDeleteModal = false">
+          <button
+            class="bg-blue-400 hover:bg-blue-500 w-1/2 rounded text-white"
+            @click="showDeleteModal = false"
+          >
             <i class="fa-solid fa-times-circle mx-1"></i> Cancel
           </button>
           <button
