74a41feeb8c5607736405eaed1de014ee1a81924
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-31 19:53:34 +0300

    restyles dashboard

--- src/domain/applications/views/RequestDetails.vue
+++ src/domain/applications/views/RequestDetails.vue
@@ -138,6 +138,41 @@ function rejectRequest() {
       </div>
     </div>
   </AppModal>
+
+
+
+
+
+  <AppModal v-model="showQueryModal" xl>
+    <div class="flex">
+      <div class="w-full">
+        <div class="flex">
+          <span class="mx-auto text-center justify-center">
+            <i
+              class="mx-auto fa-solid fa-exclamation-circle text-3xl text-danger"
+            ></i>
+          </span>
+        </div>
+        <p class="py-5 text-center">
+          Are you sure you want to reject this service request?
+        </p>
+        <div class="flex w-1/2 gap-2 justify-center mx-auto">
+          <button
+            class="bg-blue-400 hover:bg-blue-500 w-1/2 rounded text-white"
+            @click="showQueryModal = false"
+          >
+            <i class="fa-solid fa-times-circle mx-1"></i> Cancel
+          </button>
+          <button
+            class="bg-danger text-white p-1 w-1/2 rounded hover:bg-red-800"
+            @click="rejectRequest"
+          >
+            <i class="fa-solid fa-check-circle mx-1"></i> Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  </AppModal>
 </template>
 
 <style scoped>
