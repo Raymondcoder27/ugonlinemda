8d7c8fbacd6a8464486cdf93452bd46e82355aa2
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-03 12:35:19 +0300

    restyles dashboard ui

--- src/domain/applications/ServiceApplications.vue
+++ src/domain/applications/ServiceApplications.vue
@@ -499,7 +499,7 @@ watch(
               <div v-if="application.status == 'Queried'">
                 <label>
                   <span
-                    class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-blue-600 bg-blue-100 border border-blue-200 hover:text-blue-700 hover:bg-blue-200"
+                    class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-yellow-600 bg-yellow-100 border border-yellow-200 hover:text-yellow-700 hover:bg-blue-200"
                     @click="view(application)"
                     >Queried</span
                   >
